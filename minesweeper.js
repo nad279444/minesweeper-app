faeda601b297ea067a4b6ba4c2b9aa69d9f1a5ff
bernard frimpong <nardfrimpong@gmail.com>
2021-05-21 20:27:11 +0000

    completed logic

--- minesweeper.js
+++ minesweeper.js
@@ -1,5 +1,5 @@
 //logic
-const TILE_STATUSES = {
+export const TILE_STATUSES = {
     HIDDEN: 'hidden',
     MINE : 'mine',
     NUMBER: 'number',
@@ -23,7 +23,7 @@ export function createBoard(boardSize,numberOfMines){
                 element,
                 x,
                 y,
-                mine: true,
+                mine: minePositions.some(positionMatch.bind(null,{x,y})),
                 get status(){
                     return element.dataset.status
                 },
@@ -66,3 +66,49 @@ function positionMatch(a,b){
     return a.x === b.x && a.y === b.y
 }
 
+export function markTile(tile){
+    if(tile.status!==  TILE_STATUSES.HIDDEN && tile.status !== TILE_STATUSES.MARKED){
+        return
+
+    }
+    if(tile.status === TILE_STATUSES.MARKED){
+        tile.status = TILE_STATUSES.HIDDEN
+    }else {
+        tile.status = TILE_STATUSES.MARKED
+    }
+
+}
+
+ export function revealTile(board,tile){
+    if (tile.status !== TILE_STATUSES.HIDDEN){
+        return
+    }
+
+    if(tile.mine){
+        tile.status = TILE_STATUSES.MINE 
+        return
+    }else {
+        tile.status = TILE_STATUSES.NUMBER
+        const adjacentTiles = nearbyTiles(board,tile)
+        const mines = adjacentTiles.filter(t => t.mine)
+        if(mines.length === 0) {
+            adjacentTiles.forEach(revealTile.bind(null,board))
+        }else{
+            tile.element.textContent = mines.length
+        }
+    }
+
+}
+
+
+function nearbyTiles(board,{x,y}){
+    const tiles = []
+    for (let xOffset = -1;xOffset <= 1; xOffset++){
+        for (let yOffset = -1;yOffset <= 1; yOffset++){
+            const tile = board[x + xOffset][y + yOffset]
+            if (tile) tiles.push(tile)
+        }
+    }
+
+    return tiles
+}
--- script.js
+++ script.js
@@ -1,16 +1,36 @@
 //ui display
-import {createBoard} from './minesweeper.js'
+import {createBoard,markTile,revealTile,TILE_STATUSES} from './minesweeper.js'
 
 const BOARD_SIZE = 10
-const NUMBER_OF_MINES = 2
+const NUMBER_OF_MINES = 10
 
 const board = createBoard(BOARD_SIZE,NUMBER_OF_MINES)
 const boardElement = document.querySelector('.board')
+const minesLeftText = document.querySelector('[data-mine-count]')
+
+console.log(board)
 
 board.forEach(row => {
     row.forEach(tile => {
         boardElement.append(tile.element)
+        tile.element.addEventListener('click',() => {
+            revealTile(board,tile)
+        })
+        tile.element.addEventListener('contextmenu',(e) => {
+            e.preventDefault()
+            markTile(tile)
+            reduceMinesLeft()
+        })
     })
 })
 
 boardElement.style.setProperty("--size",BOARD_SIZE)
+minesLeftText.textContent = NUMBER_OF_MINES
+
+
+function reduceMinesLeft(){
+    const markedTilesCount = board.reduce((count,row) => {
+        return count + row.filter(tile => tile.status === TILE_STATUSES.MARKED).length
+    },0)
+   minesLeftText.textContent = NUMBER_OF_MINES - markedTilesCount
+}
